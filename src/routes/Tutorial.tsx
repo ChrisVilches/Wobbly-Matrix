bf98d6459fd616541a7edcfbf5a982efc4fd7bd7
Chris Vilches <felovilches@gmail.com>
2022-08-24 00:58:54 +0900

    add margin

--- src/routes/Tutorial.tsx
+++ src/routes/Tutorial.tsx
@@ -103,7 +103,7 @@ export const Tutorial = (): ReactElement => {
 
   return (
     <div className="grid grid-cols-12 xl:gap-12 gap-4">
-      <div className="md:col-span-5 col-span-12">
+      <div className="md:col-span-5 col-span-12 mb-8">
         <GridWrapper {...configurations[currentStage - 1]} frameLimit={false} />
       </div>
       <div className="md:col-span-7 col-span-12">
